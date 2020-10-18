58f39f1fe4b0601e8bba7f43fa578948472d4fc0
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-18 00:53:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
   
   get '/team' do
-    @teamname = params[:teamname]
+    @teamname = params[:name]
     @coach = params[:coach]
     @point = params[:point]
     @shooting = params[:shooting]
