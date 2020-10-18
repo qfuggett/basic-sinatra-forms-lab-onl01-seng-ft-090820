80148a048a55179737e8029cada62d1b1b3d639d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-18 00:52:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-
+  get '/newteam' do
+    
+    erb :newteam
+  end
+  
+  get '/team' do
+    @teamname = params[:teamname]
+    @coach = params[:coach]
+    @point = params[:point]
+    @shooting = params[:shooting]
+    @power = params[:power]
+    @small = params[:small]
+    @center = params[:center]
+    
+    erb :team
+  end
 end
